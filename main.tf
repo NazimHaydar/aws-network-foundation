5e6fdf2ecd93d9ff6543a18c67af48d89c877523
Nazim Haydar <nazimhaydarkh@gmail.com>
2026-07-17 03:13:58 +0300

    Add NAT Gateway, private route table, and GitHub Actions CI pipeline

--- main.tf
+++ main.tf
@@ -103,3 +103,37 @@ resource "aws_security_group" "web" {
     Name = "project1-web-sg"
   }
 }
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "project1-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "project1-nat"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "project1-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
